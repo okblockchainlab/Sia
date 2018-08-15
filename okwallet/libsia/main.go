aa5af9970c77767457dccbe3cd5e0353352a5615
fatcat22 <pandayt@163.com>
2018-08-15 15:40:09 +0800

    add jni interface

--- okwallet/libsia/main.go
+++ okwallet/libsia/main.go
@@ -1,19 +1,96 @@
 package main
 
+// #include <jni.h>
+import "C"
+
 import (
 	"gitlab.com/NebulousLabs/Sia/okwallet/okwallet"
+	"regexp"
 )
 
-func GetAddressByPrivateKey(ssk string) (address, ucs_s string, err error) {
-	return okwallet.GetAddressByPrivateKey(ssk)
+func setErrorResult(env *C.JNIEnv, errMsg string) C.jobjectArray {
+	result := newStringObjectArray(env, 1)
+	setObjectArrayStringElement(env, result, 0, errMsg)
+	return result
 }
 
-func CreateRawTransaction(amount_s string, fee_s string, from_ucs_s string, to_ucs_s string, refund_ucs_s string, spendingTxs_s string) (string, error) {
-	return okwallet.CreateRawTransaction(amount_s, fee_s, from_ucs_s, to_ucs_s, refund_ucs_s, spendingTxs_s)
+func getAddressByPrivateKeyExecute(env *C.JNIEnv, cmd string, args []string) C.jobjectArray {
+	if len(args) != 1 {
+		return setErrorResult(env, "error: "+cmd+" wrong argument count")
+	}
+
+	addr, ucs, err := okwallet.GetAddressByPrivateKey(args[0])
+	if err != nil {
+		return setErrorResult(env, "error: "+err.Error())
+	}
+
+	result := newStringObjectArray(env, 2)
+	setObjectArrayStringElement(env, result, 0, addr)
+	setObjectArrayStringElement(env, result, 1, ucs)
+
+	return result
 }
 
-func SignRawTransaction(txBuilder_s string, secKeys_s string) (string, error) {
-	return okwallet.SignRawTransaction(txBuilder_s, secKeys_s)
+func createRawTransactionExecute(env *C.JNIEnv, cmd string, args []string) C.jobjectArray {
+	if len(args) != 6 {
+		return setErrorResult(env, "error: "+cmd+" wrong argument count")
+	}
+
+	txBuilder, err := okwallet.CreateRawTransaction(args[0], args[1], args[2], args[3], args[4], args[5])
+	if err != nil {
+		return setErrorResult(env, "error: "+err.Error())
+	}
+
+	result := newStringObjectArray(env, 1)
+	setObjectArrayStringElement(env, result, 0, txBuilder)
+
+	return result
+}
+
+func SignRawTransactionExecute(env *C.JNIEnv, cmd string, args []string) C.jobjectArray {
+	if len(args) != 2 {
+		return setErrorResult(env, "error: "+cmd+" wrong argument count")
+	}
+
+	signedTx, err := okwallet.SignRawTransaction(args[0], args[1])
+	if err != nil {
+		return setErrorResult(env, "error: "+err.Error())
+	}
+
+	result := newStringObjectArray(env, 1)
+	setObjectArrayStringElement(env, result, 0, signedTx)
+
+	return result
+}
+
+//export Java_com_okcoin_vault_jni_sia_Siaj_execute
+func Java_com_okcoin_vault_jni_sia_Siaj_execute(env *C.JNIEnv, _ C.jclass, _ C.jstring, jcommand C.jstring) C.jobjectArray {
+	command, err := jstring2string(env, jcommand)
+	if err != nil {
+		return setErrorResult(env, "error: "+err.Error())
+	}
+
+	sepExp, err := regexp.Compile(`\s+`)
+	if err != nil {
+		return setErrorResult(env, "error: "+err.Error())
+	}
+
+	args := sepExp.Split(command, -1)
+	if len(args) < 2 {
+		return setErrorResult(env, "error: invalid command")
+	}
+
+	switch args[0] {
+	case "getaddressbyprivatekey":
+		return getAddressByPrivateKeyExecute(env, args[0], args[1:])
+	case "createrawtransaction":
+		return createRawTransactionExecute(env, args[0], args[1:])
+	case "signrawtransaction":
+		return SignRawTransactionExecute(env, args[0], args[1:])
+	default:
+		return setErrorResult(env, "error: unknown command: "+args[0])
+	}
+	return setErrorResult(env, "error: unknown command: "+args[0])
 }
 
 func main() {}
--- runbuild.sh
+++ runbuild.sh
@@ -1,8 +1,31 @@
 #!/bin/sh
 
+if [ -z "$JAVA_HOME" ]; then
+  printf "No JAVA_HOME detected! "
+  printf "Setup JAVA_HOME before build: export JAVA_HOME=/path/to/java\\n"
+  exit 1
+fi
+
 BUILD_TIME=`date`
 GIT_REVISION=`git rev-parse --short HEAD`
 GIT_DIRTY=`git diff-index --quiet HEAD -- || echo "✗-"`
+
+EXT=so
+TARGET_OS=`uname -s`
+case "$TARGET_OS" in
+  Darwin)
+    EXT=dylib
+    export CGO_CFLAGS="-I${JAVA_HOME}/include -I${JAVA_HOME}/include/darwin"
+    ;;
+  Linux)
+    EXT=so
+    export CGO_CFLAGS="-I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux"
+    ;;
+  *)
+  echo "Unknown platform!" >&2
+  exit 1
+esac
+
 
 # ldflags is copied from Makefile
 ldflags="-X gitlab.com/NebulousLabs/Sia/build.GitRevision=${GIT_DIRTY}${GIT_REVISION} -X \"gitlab.com/NebulousLabs/Sia/build.BuildTime=${BUILD_TIME}\""
@@ -14,4 +37,6 @@ pkgs="./build ./cmd/siac ./cmd/siad ./compatibility ./crypto ./encoding ./module
 go build -tags="netgo" -a -ldflags="-s -w ${ldflags}" ${pkgs}
 
 
-go build -o libsia.dylib -buildmode=c-shared -tags="netgo" -a -ldflags="-s -w ${ldflags}" ./okwallet/libsia
+go build -o libsia.${EXT} -buildmode=c-shared -tags="netgo" -a -ldflags="-s -w ${ldflags}" ./okwallet/libsia
+[ $? -ne 0 ] && exit 1
+nm libsia.${EXT} |grep "[ _]Java_com_okcoin"
